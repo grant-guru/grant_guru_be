1a8c5710ff70edb1564a64a710a43a546d360e6a
Drew Layton <dlayton66@gmail.com>
2023-03-30 11:44:13 -0600

    Create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.postgres import fields
 
-# Create your models here.
+class User(models.Model):
+  first_name = models.TextField()
+  last_name = models.TextField()
+  
+class Grant(models.Model):
+  title = models.TextField()
+  organization = models.TextField()
+  amount = models.IntegerField()
+  description = models.TextField()
+  deadline = models.DateField()
+  education = models.TextField()
+  state = models.TextField()
+  women = models.BooleanField()
+  lgbt = models.BooleanField()
+  ethnicity = fields.ArrayField(models.TextField(), default=list)
+  veteran = models.BooleanField()
+  immigrant = models.BooleanField()
+  url = models.URLField()
+  image_url = models.URLField()
+  users = models.ManyToManyField(User)
