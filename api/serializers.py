a104c68e609d62e160a49c644f2527f9ea1369a1
Drew Layton <dlayton66@gmail.com>
2023-03-30 11:46:22 -0600

    Create serializers, views, and urls

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from .models import Grant, User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = '__all__'
+
+class GrantSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Grant
+    fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import User, Grant
+from rest_framework import viewsets
+from api.serializers import UserSerializer, GrantSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class GrantViewSet(viewsets.ModelViewSet):
+  queryset = Grant.objects.all()
+  serializer_class = GrantSerializer
