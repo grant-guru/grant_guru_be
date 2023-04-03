60469c33ae820fbc939d1e7d0d8b8cf664829f25
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-02 19:56:44 -0600

    Create favorites)

--- api/views.py
+++ api/views.py
@@ -3,15 +3,13 @@ from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
 from api.serializers import UserSerializer, GrantSerializer
 from django.http import JsonResponse, HttpResponse
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 class GrantViewSet(viewsets.ModelViewSet):
   queryset = Grant.objects.all()
   serializer_class = GrantSerializer
 
-# @csrf_exempt
 def grant_list(request):
-  # import ipdb; ipdb.set_trace()
   if request.method == 'GET':
     grants = Grant.objects.all()
     serializer = GrantSerializer(grants, many=True)
@@ -35,15 +33,20 @@ def favorite(request, pk):
   except Grant.DoesNotExist:
     return HttpResponse(status=404)
   
+@csrf_exempt
 def favorite_create(request, pk_user, pk_grant):
   # TODO: error handling
   user = User.objects.get(pk=pk_user)
   favorite = Grant.objects.get(pk=pk_grant)
   if request.method == 'POST':
-    print("create a favorite conditional branch")
-    user.grants.add(favorite)
+    # print("create a favorite conditional branch")
+    # import ipdb; ipdb.set_trace()
+    favorite.users.add(user)
+    # user.grants.add(favorite)
     serializer = GrantSerializer(favorite, many=False)
-    return JsonResponse(serializer.data, safe=False), HttpResponse(status=201)
+    # import ipdb; ipdb.set_trace()
+    # return JsonResponse(serializer.data), HttpResponse(status=201)
+    return JsonResponse(serializer.data)
   elif request.method == 'DELETE':
     print("delete a favorite conditional branch")
     user.favorites.remove(favorite)
