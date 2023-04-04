42b65b6f77f6b3756b81d925439f8ee3448a2eec
Matisse Mallette <matissemallette@gmail.com>
2023-04-04 13:32:07 -0400

    change urls

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 urlpatterns = [
     path('api/v1/users/<int:pk_user>/scholarships/<int:pk_grant>', views.favorite_create),
-    path('api/v1/scholarships/', views.grant_list),
-    path('api/v1/scholarships/<int:pk>/', views.grant_detail),
+    path('api/v1/scholarships/', views.GrantViewSet.list),
+    path('api/v1/scholarships/<int:pk>/', views.GrantViewSet.retrieve),
     path('api/v1/users/<int:pk_user>/favorites', views.favorite),
 ]
