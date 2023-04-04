d244e7fa97d2354cf01bae6ffda1e51273964089
Drew Layton <dlayton66@gmail.com>
2023-04-04 10:56:08 -0600

    fix: Change urls to match new viewset setup

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 urlpatterns = [
     path('users/<int:pk_user>/scholarships/<int:pk_grant>', views.favorite_create),
-    path('scholarships/', views.grant_list),
-    path('scholarships/<int:pk>/', views.grant_detail),
+    path('scholarships/', views.GrantViewSet.list),
+    path('scholarships/<int:pk>/', views.GrantViewSet.retrieve),
     path('users/<int:pk_user>/favorites', views.favorite),
 ]
